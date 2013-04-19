58c0fe756e9cc3571151d91d5c82e6a33fcb464f
Aakrit <asteroidxvi@gmail.com>
2013-04-18 20:30:36 -0500

    Initial Commit

---
+++ OODesign_HW1.2_Template_Pattern/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OODesign_HW1.2_Template_Pattern
+//
+//  Created by Aakrit Prasad on 4/18/13.
+//  Copyright (c) 2013 Aakrit Prasad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
